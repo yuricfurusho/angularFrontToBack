fd831a86ee4180e68ed6ef5a82a5a4d799f0ccbe
yuricfurusho <yuricfurusho@livetouch.com.br>
2017-11-17 21:04:29 -0200

    3-14 com correcoes do lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SandboxComponent } from './components/sandbox.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SandboxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/sandbox.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox',
+  template: `<h1>Hello World</h1>`
+})
+
+export class SandboxComponent {
+
+}
