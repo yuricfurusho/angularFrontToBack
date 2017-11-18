6c10cc2e6f852b4ccb63afa3cef0f233c365f8fa
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 17:09:17 -0200

    4-22 property binding

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,25 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <!--
+    <h1>Hello World</h1>
+    &lt;!&ndash;3 ways to bind properties&ndash;&gt;
+    <div><img src="{{ imageUrl }}"></div>
+    <div><img [src]="imageUrl"></div>
+    <div><img bind-src="imageUrl"></div>
+
+    <h4>Image Location: <span [textContent]="imageUrl"></span></h4>
+    -->
+    <hr>
+    <h2>Create Post</h2>
+    <p [hidden]="isUnchanged">Post has been changed, please save</p>
+    <button [disabled]="isUnchanged">Save</button>
+
+  `
 })
 
 export class SandboxComponent {
-
+  imageUrl = 'http://lorempixel.com/400/200';
+  isUnchanged = false;
 }
