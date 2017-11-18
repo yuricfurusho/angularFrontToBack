1a6cf00c93c490c4a3519e12fa644671861c5a81
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 15:47:58 -0200

    4-19

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox',
+  templateUrl: './sandbox.component.html',
+  styleUrls: ['./sandbox.component.css']
+  // styles: [`
+  //   .special {
+  //     color: green;
+  //     font-size: 20px;
+  //     text-transform: uppercase;
+  //   }
+  // `]
+})
+
+export class SandboxComponent {
+
+}
