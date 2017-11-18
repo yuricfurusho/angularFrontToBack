e0d52ac6a5587dab28ccc5b9874a8f491eeb384b
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 18:16:05 -0200

    4-24 ngStyle binding

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,27 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <h1>Hello World</h1>
+    <div [style.font-size]="isSpecial ? 'x-large' : 'smaller'">Font size depends on isSpecial</div>
+    <div [ngStyle]="currentStyles">This div is initially saveable & special</div>
+  `
 })
 
 export class SandboxComponent {
+  isSpecial = true;
+  canSave = true;
 
+  currentStyles = {};
+
+  constructor() {
+    this.setCurrentStyles();
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'font-style': this.canSave ? 'italic' : 'normal',
+      'font-size': this.isSpecial ? '24px' : '12px'
+    };
+  }
 }
