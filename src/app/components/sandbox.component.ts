72aed9db03d5632bb1b018733eabcbe1d8f27a66
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 17:40:30 -0200

    4-23 ngClass , class binding

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,38 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <h1>Hello World</h1>
+    <h4 [class.special]="isSpecial">This class binding is special</h4>
+    <h4 [ngClass]="currentClasses">This div is initially special and saveable</h4>
+  `,
+  styles: [
+      `
+      .special {
+        color: green;
+      }
+
+      .saveable {
+        text-transform: uppercase;
+      }
+    `
+  ]
 })
 
 export class SandboxComponent {
+  isSpecial = true;
+  canSave = true;
+  currentClasses: {};
 
+  constructor() {
+    this.setCurrentClasses();
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      saveable: this.canSave,
+      special: this.isSpecial
+    };
+  }
 }
+
