0d128d154a79eca73259eed747d342665e740511
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 19:01:11 -0200

    5-27 Eventos de mouse e teclado

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,26 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <h1>Hello World</h1>
+    <button id="btn" (click)="fireEvent($event)">click Event</button>  <br>
+    <button id="btn" (mouseover)="fireEvent($event)">mouseover Event</button>  <br>
+    <button id="btn" (mousedown)="fireEvent($event)">mousedown Event</button>  <br>
+    <button id="btn" (mouseup)="fireEvent($event)">mouseup Event</button>  <br>
+    <button id="btn" (dblclick)="fireEvent($event)">dblclick Event</button>  <br>
+    <button id="btn" (drag)="fireEvent($event)">drag Event</button>  <br>
+    <button id="btn" (dragover)="fireEvent($event)">dragover Event</button>  <br>
+  `
 })
 
 export class SandboxComponent {
+  fireEvent(e) {
+    // console.log('Button clicked');
+    // console.log(greeting);
+    // console.log(e.target);
+    // console.log(e.target.id);
+    console.log(e.type);
+    // alert(e.type);
 
+  }
 }
