bf0cba1acb2dd414046e0aebfa95aca7b2b81f3f
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 16:40:27 -0200

    4-21 ngIf ngSwitch

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,33 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <h1>Hello
+      <!--
+      <span *ngIf="showName">{{name}}</span>
+      <span *ngIf="!showName">World</span>
+      -->
+      <span *ngIf="showName; else noName">{{name}}</span>
+      <ng-template #noName>World</ng-template>
+
+      <p>Hello {{ showName ? name : 'World'}}</p>
+
+    </h1>
+
+    <hr>
+
+    <div [ngSwitch]="greeting">
+      <div *ngSwitchCase="'1'">Hello World</div>
+      <div *ngSwitchCase="'2'">Hi There</div>
+      <div *ngSwitchCase="'3'">What's Up</div>
+      <div *ngSwitchDefault>Hello</div>
+    </div>
+
+  `
 })
 
 export class SandboxComponent {
-
+  name = 'John Doe';
+  showName = true;
+  greeting = -1;
 }
