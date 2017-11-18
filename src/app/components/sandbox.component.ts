89b1c58796d0941570c2e0a6aa0923d83ea73338
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-18 02:24:19 -0200

    3-16

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `<h1>Hello {{ name }}</h1>`
 })
 
 export class SandboxComponent {
+  name: string = 'John Doe';
+  age: number = 35;
+  hasChildren: boolean = true;
+  city: any = 'Boston';
+  myNumbersArray: number[] = [1, 2, 3];
+  myStringsArray: string[] = ['hello', 'world', 3];
+  myAnyArray: any[] = [1, 2, 'hello'];
+  myTuple: [string, number] = ['hello', 3];
+  unusable: void = undefined;
+  u: undefined = undefined;
+  n: null = null;
 
+  constructor() {
+    this.myNumbersArray = ['hello', 'a', 'd'];
+
+  }
 }
