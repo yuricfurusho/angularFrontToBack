0f01a47270f9ee9ae891b105a0b2daffbc137cac
yuricfurusho <yuricfurusho@livetouch.com.br>
2017-11-20 13:09:48 -0200

    5-28 changing properties with events

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,23 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <h1>Hello World</h1>
+    <button (click)="changeValue()">Change Value</button>
+    <div *ngIf="value">
+      <h1>{{ text }}</h1>
+    </div>
+
+
+  `
 })
 
 export class SandboxComponent {
+  text = 'Hello World';
+  value = true;
 
+  changeValue() {
+    // this.value = false;
+    this.value = !this.value;
+  }
 }
