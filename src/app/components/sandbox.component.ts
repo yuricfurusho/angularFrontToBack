29800d5961f20aba56ad420c5475c8b0b84c300c
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-21 00:04:13 -0200

    5-29 keyboard events and binding

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,49 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sandbox',
-  template: `<h1>Hello World</h1>`
+  template: `
+    <h1>Hello World</h1>
+    <div>
+      <input type="text" (keyup)="fireEvent($event)" placeholder="keyup event">
+    </div>
+    <div>
+      <input type="text" (keydown)="fireEvent($event)" placeholder="keydown event">
+    </div>
+    <div>
+      <input type="text" (focus)="fireEvent($event)" placeholder="focus event">
+    </div>
+    <div>
+      <input type="text" (blur)="fireEvent($event)" placeholder="blur event">
+    </div>
+    <div>
+      <input type="text" (cut)="fireEvent($event)" placeholder="cut event">
+    </div>
+    <div>
+      <input type="text" (copy)="fireEvent($event)" placeholder="copy event">
+    </div>
+    <div>
+      <input type="text" (paste)="fireEvent($event)" placeholder="paste event">
+    </div>
+
+    <hr>
+
+    <div>
+      <input type="text" (keyup)="changeText($event)" placeholder="change text" value="{{text}}">
+    </div>
+
+    <h2>{{ text }}</h2>
+  `
 })
 
 export class SandboxComponent {
+  text = 'Hello';
 
+
+  fireEvent(e) {
+    console.log(e.type);
+  }
+
+  changeText(e) {
+    this.text = e.target.value;
+  }
 }
