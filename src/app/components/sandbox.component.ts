e7729c7436905dc356d30e6d8d20ff1203b22dd0
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-25 17:44:53 -0200

    6-38 HTTP POST, MAPPING, OBSERVABLE, BINDING

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -5,6 +5,22 @@ import {DataService} from '../services/data.service';
   selector: 'app-sandbox',
   template: `
     <h1>Hello World</h1>
+    <form (submit)="onSubmit()">
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control" [(ngModel)]="user.name" name="name">
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text" class="form-control" [(ngModel)]="user.email" name="email">
+      </div>
+      <div class="form-group">
+        <label>Phone</label>
+        <input type="text" class="form-control" [(ngModel)]="user.phone" name="phone">
+      </div>
+      <input type="submit" class="btn btn-success" value="Submit">
+    </form>
+
     <div *ngFor="let user of users">
       <div class="well">
         <ul class="list-group">
@@ -20,11 +36,23 @@ import {DataService} from '../services/data.service';
 
 export class SandboxComponent {
   users: any[];
-
+  user = {
+    name: '',
+    username: '',
+    email: '',
+    phone: ''
+  };
   constructor(public dataService: DataService) {
     this.dataService.getUsers().subscribe(users => {
       // console.log(users);
       this.users = users;
+    });
+  }
+
+  onSubmit() {
+    this.dataService.addUser(this.user).subscribe(user => {
+      console.log(user);
+      this.users.unshift(user);
     });
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,4 +13,9 @@ export class DataService {
     return this.http.get('http://jsonplaceholder.typicode.com/users')
       .map(res => res.json());
   }
+
+  addUser(user) {
+    return this.http.post('http://jsonplaceholder.typicode.com/users', user)
+      .map(res => res.json());
+  }
 }
